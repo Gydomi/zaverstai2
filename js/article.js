0ceb1f6894eceb6ed94166dcccf241a20754426e
GanievStas <benony0gmail.com>
2020-10-10 07:22:51 +1000

    Добавление комментариев

--- js/article.js
+++ js/article.js
@@ -27,3 +27,38 @@ itemMessage.oninput = function () {
     btnWriteMe.disabled = false;
   }
 };
+
+let formPost = document.querySelector(".comments__form");
+let btnPost = document.querySelector('.btn__post-js');
+let listComments = document.querySelector(".comments__list");
+let itemName = document.querySelector(".form-post__name-input");
+let itemEmail = document.querySelector(".form-post__email-input");
+
+function createEvement (nameTag, classes, content = undefined) {
+  let newItem = document.createElement(nameTag);
+  for (let className of classes) {
+    newItem.classList.add(className);
+  }
+  if (content !== undefined) {
+    newItem.textContent = content;
+  }
+  return newItem;
+}
+
+formPost.onsubmit = function(evt) {
+
+  evt.preventDefault();
+
+  let newComment = createEvement('li', ['comments__item', 'comment']);
+  let commentWrap = createEvement('div', ['comment__wrap']);
+
+  commentWrap.append(createEvement('img', ['comment__avatar']));
+  commentWrap.append(createEvement('p', ['comment__name'], itemName.value));
+  newComment.append(commentWrap);
+  newComment.append(createEvement('div', ['comment__text'], itemMessage.value));
+
+  listComments.append(newComment);
+
+  btnWriteMe.disabled = true;
+
+};
